26cd53281d7cecf9c9684a18dcf8ff28edfc9ddf
Nikos Linakis <nlinakis@gmail.com>
2018-02-26 08:33:17 +0200

    Optimized span generation to only happen if applied style differs

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ function layer(context, layer) {
     var output = 'SpannableString spString = new SpannableString(' + JSON.stringify(layer.content) + ');\n'
 
     for (var i in layer.textStyles) {
-        output += spannify(layer.textStyles[i].range, layer.textStyles[i].textStyle);
+        var previousTextStyle = i > 0 ? layer.textStyles[i - 1].textStyle : null;
+        output += spannify(layer.textStyles[i].range, layer.textStyles[i].textStyle, previousTextStyle);
     }
 
     return {
@@ -15,15 +16,23 @@ function layer(context, layer) {
     };
 };
 
-function spannify(range, textStyle) {
+function spannify(range, textStyle, appliedTextStyle) {
+
+    appliedTextStyle = appliedTextStyle || {};
 
     var ret = "";
 
-    ret += 'spString.setSpan(new ForegroundColorSpan(Color.parseColor("' + colorToHex(textStyle.color) + '")), ' + range.start + ', ' + range.end + ', Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);\n'
+    if (compareColors(textStyle.color, appliedTextStyle.color)) {
+        ret += 'spString.setSpan(new ForegroundColorSpan(Color.parseColor("' + colorToHex(textStyle.color) + '")), ' + range.start + ', ' + range.end + ', Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);\n'
+    }
 
-    ret += 'spString.setSpan(new TypefaceSpan("' + textStyle.fontFace + '"), ' + range.start + ', ' + range.end + ', Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);\n'
+    if (textStyle.fontFace !== appliedTextStyle.fontFace) {
+        ret += 'spString.setSpan(new TypefaceSpan("' + textStyle.fontFace + '"), ' + range.start + ', ' + range.end + ', Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);\n'
+    }
 
-    ret += 'spString.setSpan(new AbsoluteSizeSpan("' + textStyle.fontSize + '"), ' + range.start + ', ' + range.end + ', Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);\n'
+    if (textStyle.fontSize !== appliedTextStyle.fontSize) {
+        ret += 'spString.setSpan(new AbsoluteSizeSpan(' + Math.round(textStyle.fontSize) + '), ' + range.start + ', ' + range.end + ', Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);\n'
+    }
 
     if (textStyle.textAlign !== "center") {
         ret += 'spString.setSpan(new AlignmentSpan.Standard(Layout.Alignment.ALIGN_CENTER), ' + range.start + ', ' + range.end + ', Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);\n'
@@ -34,11 +43,27 @@ function spannify(range, textStyle) {
     return ret;
 }
 
+function compareColors(colorA, colorB) {
+    if (!colorA || !colorB) {
+        return false
+    }
 
+    return colorA.a === colorB.a
+        && colorA.r === colorB.r
+        && colorA.g === colorB.g
+        && colorA.b === colorB.b
+}
+
+// no point in showing Spannable logic
+// if we have a single textStyle in the
+// entire label. That's what the xml is for.
 function shouldShowExtension(layer) {
     return layer.textStyles.length > 1
 }
 
 function colorToHex(color) {
-    return "#" + ((1 << 24) + (color.r << 16) + (color.g << 8) + color.b).toString(16).slice(1);
+    if (color) {
+        return "#" + ((1 << 24) + (color.r << 16) + (color.g << 8) + color.b).toString(16).slice(1);
+    }
+    return ""
 }
