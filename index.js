11c590036bd9771bea6e2a21542f5fa1e10d4a1e
Nikos Linakis <nlinakis@gmail.com>
2018-02-26 20:37:46 +0200

    Use a super span for styles that apply to the entire text + minor refactoring

--- index.js
+++ index.js
@@ -3,62 +3,70 @@ function layer(context, layer) {
         return
     }
 
-    var output = 'SpannableString spString = new SpannableString(' + JSON.stringify(layer.content) + ');\n'
+    var output = 'SpannableString spString = new SpannableString(' + JSON.stringify(layer.content) + ');\n';
+    var superTextStyle = computeSuperTextStyle(layer);
+
+    output += spannify(superTextStyle.range, superTextStyle.textStyle, null, {});
 
     for (var i in layer.textStyles) {
         var previousTextStyle = i > 0 ? layer.textStyles[i - 1].textStyle : null;
-        output += spannify(layer.textStyles[i].range, layer.textStyles[i].textStyle, previousTextStyle);
+        output += spannify(layer.textStyles[i].range, layer.textStyles[i].textStyle, previousTextStyle, superTextStyle.textStyle);
     }
 
     return {
         code: output,
         language: "java"
     };
-};
-
-function spannify(range, textStyle, appliedTextStyle) {
+}
+function spannify(range, textStyle, appliedTextStyle, superTextStyle) {
 
     appliedTextStyle = appliedTextStyle || {};
 
     var ret = "";
 
-    if (compareColors(textStyle.color, appliedTextStyle.color)) {
+    if (!compare(textStyle, superTextStyle, "color") && !compare(textStyle, appliedTextStyle, "color")) {
         ret += 'spString.setSpan(new ForegroundColorSpan(Color.parseColor("' + colorToHex(textStyle.color) + '")), ' + range.start + ', ' + range.end + ', Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);\n'
     }
 
-    if (textStyle.fontFace !== appliedTextStyle.fontFace) {
+    if (!compare(textStyle, superTextStyle, "fontFace") && !compare(textStyle, appliedTextStyle, "fontFace")) {
         ret += 'spString.setSpan(new TypefaceSpan("' + textStyle.fontFace + '"), ' + range.start + ', ' + range.end + ', Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);\n'
     }
 
-    if (textStyle.fontSize !== appliedTextStyle.fontSize) {
+    if (!compare(textStyle, superTextStyle, "fontSize") && !compare(textStyle, appliedTextStyle, "fontSize")) {
         ret += 'spString.setSpan(new AbsoluteSizeSpan(' + Math.round(textStyle.fontSize) + '), ' + range.start + ', ' + range.end + ', Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);\n'
     }
 
-    if (textStyle.textAlign !== "center") {
-        ret += 'spString.setSpan(new AlignmentSpan.Standard(Layout.Alignment.ALIGN_CENTER), ' + range.start + ', ' + range.end + ', Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);\n'
-    }
-
-    // TODO: UnderlineSpan, BackgroundColorSpan, StrikethroughSpan, StyleSpan, LineHeightSpan
+    // TODO: AlignmentSpan -> textAlign,
+    // TODO: UnderlineSpan -> fontStyle,
+    // TODO: BackgroundColorSpan -> ?,
+    // TODO: StrikethroughSpan -> ?,
+    // TODO: LineHeightSpan -> lineHeight
+    // TODO: StyleSpan -> weightText
+    // TODO: ? -> fontStretch
 
     return ret;
 }
 
-function compareColors(colorA, colorB) {
-    if (!colorA || !colorB) {
-        return false
+function compare(left, right, attr) {
+
+    left = left || {};
+    right = right || {};
+
+    if(attr == "color" && left[attr] != null && right[attr] != null) {
+        return left[attr].a === right[attr].a
+            && left[attr].r === right[attr].r
+            && left[attr].g === right[attr].g
+            && left[attr].b === right[attr].b
     }
 
-    return colorA.a === colorB.a
-        && colorA.r === colorB.r
-        && colorA.g === colorB.g
-        && colorA.b === colorB.b
+    return left[attr] == right[attr];
 }
 
 // no point in showing Spannable logic
 // if we have a single textStyle in the
 // entire label. That's what the xml is for.
 function shouldShowExtension(layer) {
-    return layer.textStyles.length > 1
+    return layer.textStyles != null && layer.textStyles.length > 1
 }
 
 function colorToHex(color) {
@@ -66,4 +74,45 @@ function colorToHex(color) {
         return "#" + ((1 << 24) + (color.r << 16) + (color.g << 8) + color.b).toString(16).slice(1);
     }
     return ""
+}
+
+
+function computeSuperTextStyle(layer) {
+    if (!shouldShowExtension) {
+        return {}
+    }
+
+    // initialize with our initial textStyle
+    // every-time we find a difference in one of the attributes
+    // we will exclude it from our output.
+    // this way we will end up with a an object
+    // that contains only the attributes that are common between all
+    // of our textStyles. We also hard code the range to include the entire length
+    var superTextStyle = {
+        range: {
+            start: 0,
+            end: layer.content.length
+        },
+        textStyle: {
+            fontSize: layer.textStyles[0].textStyle.fontSize,
+            fontFace: layer.textStyles[0].textStyle.fontFace,
+            color: layer.textStyles[0].textStyle.color
+        }
+    };
+
+    for (var i=0; i < layer.textStyles.length; ++i) {
+        if (!compare(superTextStyle.textStyle, layer.textStyles[i].textStyle, "fontSize")) {
+            delete superTextStyle.textStyle.fontSize
+        }
+
+        if (!compare(superTextStyle.textStyle, layer.textStyles[i].textStyle, "fontFace")) {
+            delete superTextStyle.textStyle.fontFace
+        }
+
+        if (!compare(superTextStyle.textStyle, layer.textStyles[i].textStyle, "color")) {
+            delete superTextStyle.textStyle.color
+        }
+    }
+
+    return superTextStyle;
 }
